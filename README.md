f28729313a8512df67849b7f424823cd9aa7d444
Michał Seta <djiamnot@gmail.com>
2018-09-29 16:39:14 -0400

    Illustrate a second problem. Updateing viewport texture...

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Tell me why
 
-In this example, the viewport texture is not appearing on the plane in render/runtime while I can see it in the editor. Why?
+In this example, we use a Viewport to read a 2D label and we map the resulting texture on a MeshInstance.
+This is saved as a separate scene. Then we instantiate several copies of this scene and we fetch the reference
+to this instance. In the rendered version, all instances seem to have the same ID, yet the `print` statement
+shows us something else.
+
+Any pointers to what I'm doing wrong?
 
 Mandatory screenshot:
 
--- VP.gd
+++ VP.gd
@@ -1,8 +1,25 @@
 extends Spatial
 
-func _ready():
-    set_process(true)
+onready var mesh = $"MeshInstance"
+onready var label = $"Viewport/Label"
+onready var thisroot = $"."
 
+func _ready():
+	set_process(true)
+
+	# Get the viewport and clear it
+	var viewport = get_node("Viewport")
+	viewport.set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
+
+	# Let two frames pass to make sure the vieport's is captured
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+	mesh.material_override.set_texture(0, viewport.get_texture())
+
+	# Note here, that thisroot id is not the same in the rendered viewport texture
+	# as printed. In fact, when rendered in the mainloop they all have the same ID
+	print(" *** ", thisroot)
+	label.text = str(thisroot)
 
 func _process(delta):
-    rotate_x(delta)
+	rotate_x(delta*0.1)
